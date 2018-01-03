34740b45d6058fac248dc1c0a90fba501d7173d2
Grzegorz Slazak <gregorysl@gmail.com>
2018-01-03 23:22:05 +0100

    fix undefined regeneratorRuntime

--- components/App.js
+++ components/App.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { render } from "react-dom";
 import Table from './Table'
 
-
-
 class App extends React.Component {
   constructor() {
     super();
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import './styles/style.scss';
+import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';  
