273fb6fddf873ac44a149834db6de3c053c7075e
Grzegorz Slazak <gregorysl@gmail.com>
2018-01-10 21:23:08 +0100

    Fixed moment

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ import moment from 'moment';
 moment.locale('en-gb');
 import 'moment/locale/en-gb'
 import 'moment/src/locale/en-gb'
-import enUS from 'antd/lib/locale-provider/en_GB';
+import enUS from 'antd/es/locale-provider/en_GB'
 import 'antd/dist/antd.less';
   
 const rootDiv = document.getElementById('app');
