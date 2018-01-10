ea92c4168b80436c7312217d1b837c599184d903
Grzegorz Slazak <gregorysl@gmail.com>
2018-01-10 20:38:18 +0100

    add antd

--- index.js
+++ index.js
@@ -5,13 +5,23 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store';
 import App from './components/App';
+import { LocaleProvider } from 'antd';
+import moment from 'moment';
+moment.locale('en-gb');
+import 'moment/locale/en-gb'
+import 'moment/src/locale/en-gb'
+import enUS from 'antd/lib/locale-provider/en_GB';
+import 'antd/dist/antd.less';
   
 const rootDiv = document.getElementById('app');
 
 render( 
+  
+  <LocaleProvider locale={enUS}>
   <Provider store={store}>
     <App/>
-  </Provider>, rootDiv);
+  </Provider>
+  </LocaleProvider>, rootDiv);
 
 if (module.hot) {
   module.hot.accept();
