a69c67e6c6d813e8ef83032dd9067f3e0ff1c232
gregorysl <gregorysl@gmail.com>
2018-01-15 00:47:37 +0100

    Add eslint

--- components/Table.js
+++ components/Table.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { render } from "react-dom";
 import { connect } from 'react-redux';
-import 'react-table/react-table.css'
 import { selectImageAction } from '../actions/actions'
 
 import { Table } from 'antd';
