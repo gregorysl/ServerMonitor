28acf6948c08aafc7cdfd24add107a1d293bd528
gregorysl <gregorysl@gmail.com>
2018-01-27 22:32:12 +0100

    Remove locale, as it's not working for now with parcel

--- index.js
+++ index.js
@@ -1,28 +1,20 @@
 
-import 'moment/locale/en-gb';
-import 'moment/src/locale/en-gb';
-import enUS from 'antd/es/locale-provider/en_GB';
+
 import 'antd/dist/antd.less';
 import 'babel-polyfill';
 import React from 'react';
-import { LocaleProvider } from 'antd';
-import moment from 'moment';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store';
 import App from './components/App';
 import './styles/style.less';
 
-moment.locale('en-gb');
-
 
 const rootDiv = document.getElementById('app');
 const Item = (
-  <LocaleProvider locale={enUS}>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </LocaleProvider>);
+  <Provider store={store}>
+    <App />
+  </Provider>);
 render(Item, rootDiv);
 
 if (module.hot) {
