7ce22102bbe44a353222d81150e3b3621274ae3d
gregorysl <gregorysl@gmail.com>
2018-02-03 21:00:18 +0100

    Cleanup app component

--- components/App.js
+++ components/App.js
@@ -1,17 +1,12 @@
 import React from 'react';
-import SimpleTable from './Table';
+import IisTable from './Iis/IisTable';
 import ServicesList from './servicesList';
 import Hardware from './Hardware';
 
 const App = () => (
   <div>
-    {/* <Row>
-      <Col span={8}><Chart title="Processor" percent={12} /></Col>
-      <Col span={8}><Chart title="HDD" percent={75} /></Col>
-      <Col span={8}><Chart title="Ram" percent={23} /></Col>
-    </Row> */}
     <Hardware />
-    <SimpleTable />
+    <IisTable />
     <ServicesList />
   </div>
 );
