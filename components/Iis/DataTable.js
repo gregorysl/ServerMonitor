16f0bedf1ceec1b1e6a6d16917cb4e4cb402beee
gregorysl <gregorysl@gmail.com>
2018-02-20 00:18:38 +0100

    Ability to add additional columns

--- components/Iis/DataTable.js
+++ components/Iis/DataTable.js
@@ -3,24 +3,31 @@ import { Table } from 'antd';
 import PropTypes from 'prop-types';
 
 
-const DataTable = ({ data, columns, message }) => {
+const DataTable = ({
+  data, columns, message, extraColumns = []
+}) => {
   if (data.length === 0 && columns.length === 0) {
     return (<h1>{message}</h1>);
   }
+  const finalColumns = [...columns, ...extraColumns];
   return (
     <Table
-      columns={columns}
+      columns={finalColumns}
       dataSource={data}
       pagination={false}
     />
   );
 };
 
+DataTable.defaultProps = {
+  extraColumns: []
+};
 
 DataTable.propTypes = {
   data: PropTypes.arrayOf(PropTypes.object).isRequired,
   columns: PropTypes.arrayOf(PropTypes.object).isRequired,
-  message: PropTypes.string.isRequired
+  message: PropTypes.string.isRequired,
+  extraColumns: PropTypes.arrayOf(PropTypes.object)
 };
 
 export default DataTable;
