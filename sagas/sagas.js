24e955169f6a1212d71fcb7395d8546249d3cfc6
gregorysl <gregorysl@gmail.com>
2018-02-23 23:19:21 +0100

    Add axios; fix sagas

--- sagas/sagas.js
+++ sagas/sagas.js
@@ -1,15 +1,17 @@
-import { all, fork } from 'redux-saga/effects';
+import { fork } from 'redux-saga/effects';
 import * as watch from './watcher';
 
+const sagas = [
+  fork(watch.watchGetServices),
+  fork(watch.watchGetHardware),
+  fork(watch.watchGetIis),
+  fork(watch.watchSetIis),
+  fork(watch.watchDiskUsage),
+  fork(watch.watchTasks),
+  fork(watch.watchSessions),
+  fork(watch.watchOracle)
+];
+
 export default function* () {
-  yield all([
-    fork(watch.watchGetServices),
-    fork(watch.watchGetHardware),
-    fork(watch.watchGetIis),
-    fork(watch.watchSetIis),
-    fork(watch.watchDiskUsage),
-    fork(watch.watchTasks),
-    fork(watch.watchSessions),
-    fork(watch.watchOracle)
-  ]);
+  yield sagas;
 }
