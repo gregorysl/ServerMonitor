c0503501952ce900854f1004c5323b9367cc4992
unknown <gregorysl@gmail.com>
2018-03-16 12:03:55 +0100

    Getting faultu url's status and error message

--- _server/ServerMonitor/Controllers/LinksController.cs
+++ _server/ServerMonitor/Controllers/LinksController.cs
@@ -1,7 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Configuration;
 using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 using System.Web.Mvc;
 using Newtonsoft.Json;
 using ServerMonitor.Helpers;
@@ -18,29 +21,61 @@ namespace ServerMonitor.Controllers
 
             foreach (LinkItem e in config.Instances)
             {
-                var credentials = !string.IsNullOrWhiteSpace(e.Username) && !string.IsNullOrWhiteSpace(e.Password)
+                var credentials = !String.IsNullOrWhiteSpace(e.Username) && !String.IsNullOrWhiteSpace(e.Password)
                     ? new NetworkCredential(e.Username, e.Password)
                     : null;
-
-                var response = ApiClient.Execute(e.Url, HttpMethod.Get, credentials);
-                links.Add(new Link
+                var link = new Link
                 {
                     Name = e.Name,
-                    Status = (int) response.StatusCode,
-                    Working = response.IsSuccessStatusCode
-                });
+                    Url = e.Url
+                };
+                try
+                {
+                    using (var handler = new HttpClientHandler { Credentials = credentials })
+                    {
+                        using (var client = new HttpClient(handler))
+                        {
+                            ServicePointManager.ServerCertificateValidationCallback +=
+                                (sender, cert, chain, sslPolicyErrors) => true;
+                            client.BaseAddress = new Uri(e.Url, UriKind.Absolute);
+                            client.DefaultRequestHeaders.Accept.Clear();
+                            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                            var response = client.GetAsync(client.BaseAddress).Result;
+                            link.Message = response.StatusCode.ToString();
+                            link.Working = response.IsSuccessStatusCode;
+                        }
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    link.Working = false;
+                    link.Message = GatherExceptions(ex);
+                }
+
+                links.Add(link);
             }
 
             return links.ToJsonResult();
         }
+        
+
+        public static string GatherExceptions(Exception e)
+        {
+            string exception = $"{e.Message}\\r\\n";
+            return e.InnerException != null ? exception + GatherExceptions(e.InnerException) : exception;
+        }
     }
+
     public class Link
     {
         [JsonProperty("key")]
         public string Name { get; set; }
-        [JsonProperty("status")]
-        public int Status { get; set; }
         [JsonProperty("working")]
         public bool Working { get; set; }
+        [JsonProperty("message")]
+        public string Message { get; set; }
+        [JsonProperty("url")]
+        public string Url { get; set; }
     }
 }
