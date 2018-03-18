216dc7045f664730554aa14bdf95c205d3431491
unknown <gregorysl@gmail.com>
2018-03-19 00:06:59 +0100

    cleanup web.config, move CORS there

--- _server/ServerMonitor/Global.asax.cs
+++ _server/ServerMonitor/Global.asax.cs
@@ -17,15 +17,5 @@ namespace ServerMonitor
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
-        protected void Application_BeginRequest()
-        {
-            string[] allowedOrigin = new string[] { "http://localhost:8080" };
-            var origin = HttpContext.Current.Request.Headers["Origin"];
-            if (origin != null && allowedOrigin.Contains(origin))
-            {
-                HttpContext.Current.Response.AddHeader("Access-Control-Allow-Origin", origin);
-                HttpContext.Current.Response.AddHeader("Access-Control-Allow-Methods", "GET,POST");
-            }
-        }
     }
 }
