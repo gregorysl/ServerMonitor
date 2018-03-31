b41511cbe0dc16a347bea370ba86b8b672892be3
unknown <gregorysl@gmail.com>
2018-03-31 21:48:57 +0200

    configure production webpack config

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -1,43 +1,40 @@
 const { resolve } = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const config = {
   devtool: 'cheap-module-source-map',
 
   entry: [
-    './main.js',
-    './assets/scss/main.scss',
+    './index.js',
+    './styles/style.less'
   ],
-
-  context: resolve(__dirname, 'app'),
 
   output: {
     filename: 'bundle.js',
-    path: resolve(__dirname, 'dist'),
-    publicPath: '',
+    path: resolve(__dirname, '_server\\ServerMonitor'),
+    publicPath: ''
   },
+
+  context: resolve(__dirname, '.'),
 
   plugins: [
     new webpack.optimize.ModuleConcatenationPlugin(),
     new HtmlWebpackPlugin({
-      template: `${__dirname}/app/index.html`,
+      template: `${__dirname}/build/index.html`,
       filename: 'index.html',
-      inject: 'body',
+      inject: 'body'
     }),
     new webpack.optimize.OccurrenceOrderPlugin(),
     new webpack.LoaderOptionsPlugin({
       minimize: true,
-      debug: false,
+      debug: false
     }),
     new webpack.optimize.UglifyJsPlugin({
-      beautify: false,
+      beautify: false
     }),
-    new webpack.DefinePlugin({ 'process.env': { NODE_ENV: JSON.stringify('production') } }),
-    new ExtractTextPlugin({ filename: './styles/style.css', disable: false, allChunks: true }),
-    new CopyWebpackPlugin([{ from: './vendors', to: 'vendors' }]),
+    new webpack.DefinePlugin({ 'process.env': { NODE_ENV: JSON.stringify('production') } })
   ],
 
   module: {
@@ -45,7 +42,7 @@ const config = {
       {
         test: /\.js?$/,
         exclude: /node_modules/,
-        loader: 'babel-loader',
+        loader: 'babel-loader'
       },
       {
         test: /\.scss$/,
@@ -54,10 +51,19 @@ const config = {
           fallback: 'style-loader',
           use: [
             'css-loader',
-            { loader: 'sass-loader', query: { sourceMap: false } },
+            { loader: 'sass-loader', query: { sourceMap: false } }
           ],
-          publicPath: '../',
-        }),
+          publicPath: '../'
+        })
+      }, {
+        test: /\.less$/,
+        use: [{
+          loader: 'style-loader' // creates style nodes from JS strings
+        }, {
+          loader: 'css-loader' // translates CSS into CommonJS
+        }, {
+          loader: 'less-loader' // compiles Less to CSS
+        }]
       },
       {
         test: /\.(png|jpg|gif)$/,
@@ -67,10 +73,10 @@ const config = {
             options: {
               limit: 8192,
               mimetype: 'image/png',
-              name: 'images/[name].[ext]',
-            },
-          },
-        ],
+              name: 'images/[name].[ext]'
+            }
+          }
+        ]
       },
       {
         test: /\.eot(\?v=\d+.\d+.\d+)?$/,
@@ -78,10 +84,10 @@ const config = {
           {
             loader: 'file-loader',
             options: {
-              name: 'fonts/[name].[ext]',
-            },
-          },
-        ],
+              name: 'fonts/[name].[ext]'
+            }
+          }
+        ]
       },
       {
         test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
@@ -91,10 +97,10 @@ const config = {
             options: {
               limit: 8192,
               mimetype: 'application/font-woff',
-              name: 'fonts/[name].[ext]',
-            },
-          },
-        ],
+              name: 'fonts/[name].[ext]'
+            }
+          }
+        ]
       },
       {
         test: /\.[ot]tf(\?v=\d+.\d+.\d+)?$/,
@@ -104,10 +110,10 @@ const config = {
             options: {
               limit: 8192,
               mimetype: 'application/octet-stream',
-              name: 'fonts/[name].[ext]',
-            },
-          },
-        ],
+              name: 'fonts/[name].[ext]'
+            }
+          }
+        ]
       },
       {
         test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
@@ -117,13 +123,13 @@ const config = {
             options: {
               limit: 8192,
               mimetype: 'image/svg+xml',
-              name: 'images/[name].[ext]',
-            },
-          },
-        ],
-      },
-    ],
-  },
+              name: 'images/[name].[ext]'
+            }
+          }
+        ]
+      }
+    ]
+  }
 };
 
 module.exports = config;
