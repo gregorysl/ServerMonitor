b4ad28353b0bbd1dff85ad5519730bf821a673f8
unknown <gregorysl@gmail.com>
2018-04-10 00:01:46 +0200

    Edit buttons for new button type

--- components/Iis/WhitelistButton.js
+++ components/Iis/WhitelistButton.js
@@ -1,15 +1,13 @@
 import React from 'react';
-import { Icon, Tooltip } from 'antd';
 import PropTypes from 'prop-types';
+import TooltipButon from '../TooltipButon';
 
 
 const WhitelistButton = (props) => {
   const title = props.whitelisted ? 'Remove from whitelist' : 'Whitelist';
   const icon = props.whitelisted ? 'star' : 'star-o';
   return (
-    <Tooltip title={title} >
-      <Icon onClick={props.click} type={icon} />
-    </Tooltip>
+    <TooltipButon title={title} click={props.click} icon={icon} />
   );
 };
 
