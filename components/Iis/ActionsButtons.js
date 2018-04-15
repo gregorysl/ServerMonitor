b6a8297d3467d170ada7c57df8c269678b44c52d
unknown <gregorysl@gmail.com>
2018-04-15 22:10:50 +0200

    use new component for build notes

--- components/Iis/ActionsButtons.js
+++ components/Iis/ActionsButtons.js
@@ -2,9 +2,10 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import StartStopButton from './StartStopButton';
-import { setIisAction, whitelistApp } from '../../actions/actions';
+import { setIisAction, whitelistApp, setNote } from '../../actions/actions';
 import WhitelistButton from './WhitelistButton';
-import TooltipButon from '../TooltipButon';
+import NoteControl from './NoteControl';
+
 
 const flatten = list => list.reduce((a, b) => a.concat(Array.isArray(b) ? flatten(b) : b), []);
 
@@ -12,11 +13,12 @@ const ActionsButtons = (props) => {
   const onSet = () => props.set(props);
   const onWhitelist = () => props.whitelist(props);
   return (
-    <React.Fragment>
+    <div className="actions-main">
       <StartStopButton running={props.running} click={onSet} />
       <WhitelistButton whitelisted={props.whitelisted} click={onWhitelist} />
-      <TooltipButon title="notes" icon="tag" />
-    </React.Fragment>);
+      <NoteControl click={props.saveNote} note={props.note} name={props.name} />
+    </div>
+  );
 };
 
 const mapDispatchToProps = dispatch => ({
@@ -35,14 +37,24 @@ const mapDispatchToProps = dispatch => ({
       isWhitelisted: item.whitelisted
     };
     dispatch(whitelistApp(data));
+  },
+  saveNote: (data) => {
+    dispatch(setNote(data));
   }
 });
+
+ActionsButtons.defaultProps = {
+  note: ''
+};
 
 ActionsButtons.propTypes = {
   running: PropTypes.bool.isRequired,
   whitelisted: PropTypes.bool.isRequired,
+  note: PropTypes.string,
+  name: PropTypes.string.isRequired,
   set: PropTypes.func.isRequired,
-  whitelist: PropTypes.func.isRequired
+  whitelist: PropTypes.func.isRequired,
+  saveNote: PropTypes.func.isRequired
 };
 
 export default connect(null, mapDispatchToProps)(ActionsButtons);
