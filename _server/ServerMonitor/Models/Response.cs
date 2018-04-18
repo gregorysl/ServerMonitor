c400d3af3083718aced61eb91489bb5c4c8fc3e2
unknown <gregorysl@gmail.com>
2018-04-18 20:50:19 +0200

    add base response class

---
+++ _server/ServerMonitor/Models/Response.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace ServerMonitor.Models
+{
+    public class Response
+    {
+        [JsonConverter(typeof(StringEnumConverter))]
+        public Status Status { get; set; }
+        public object Data { get; set; }
+        public List<Notification> Notifications { get; set; } = new List<Notification>();
+    }
+
+    public class Notification
+    {
+        [JsonConverter(typeof(StringEnumConverter))]
+        public Status Status { get; set; }
+        public string Message { get; set; }
+        public string MessageDetails { get; set; }
+    }
+
+    public enum Status
+    {
+        Success,
+        Error
+    }
+}
