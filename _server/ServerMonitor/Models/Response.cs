4afe00a024a2976b66faaa77f49e5c58a6b61a4f
unknown <gregorysl@gmail.com>
2018-04-25 21:25:25 +0200

    add helper methods to response

--- _server/ServerMonitor/Models/Response.cs
+++ _server/ServerMonitor/Models/Response.cs
@@ -8,8 +8,29 @@ namespace ServerMonitor.Models
     {
         [JsonConverter(typeof(StringEnumConverter))]
         public Status Status { get; set; }
+
         public object Data { get; set; }
         public List<Notification> Notifications { get; set; } = new List<Notification>();
+
+        public void AddSuccessNotification(string message, string messageDetails = "")
+        {
+            AddNotification(Status.Success, message, messageDetails);
+        }
+
+        public void AddErrorNotification(string message, string messageDetails = "")
+        {
+            AddNotification(Status.Success, message, messageDetails);
+        }
+
+        private void AddNotification(Status status, string message, string messageDetails)
+        {
+            Notifications.Add(new Notification
+            {
+                Status = status,
+                Message = message,
+                MessageDetails = messageDetails
+            });
+        }
     }
 
     public class Notification
