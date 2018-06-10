a3696151a084a05b687a349732a5e4e1c754f2ea
unknown <gregorysl@gmail.com>
2018-06-10 20:59:33 +0200

    Extract class to model

---
+++ _server/ServerMonitor/Models/IssToggleConfig.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace ServerMonitor.Models
+{
+    public class IssToggleConfig
+    {
+        public IssToggleConfig(List<string> appPools, bool condition)
+        {
+            AppPools = appPools;
+            Condition = condition;
+        }
+
+        public List<string> AppPools { get; }
+        public bool Condition { get; }
+    }
+}
