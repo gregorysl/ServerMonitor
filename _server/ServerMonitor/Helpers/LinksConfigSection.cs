69b0968b31f075481d13c21874535c094478d580
unknown <gregorysl@gmail.com>
2018-07-11 22:39:21 +0200

    fix r# warnings, add Type

--- _server/ServerMonitor/Helpers/LinksConfigSection.cs
+++ _server/ServerMonitor/Helpers/LinksConfigSection.cs
@@ -18,7 +18,8 @@ namespace ServerMonitor.Helpers
             return new Link
             {
                 Name = link.Name,
-                Url = link.Url
+                Url = link.Url, 
+                Type = link.Type
             };
         }
     }
@@ -27,8 +28,8 @@ namespace ServerMonitor.Helpers
         [ConfigurationProperty("", IsRequired = true, IsDefaultCollection = true)]
         public LinkCollection Instances
         {
-            get { return (LinkCollection) this[""]; }
-            set { this[""] = value; }
+            get => (LinkCollection) this[""];
+            set => this[""] = value;
         }
     }
     public class LinkCollection : ConfigurationElementCollection
@@ -49,36 +50,36 @@ namespace ServerMonitor.Helpers
         [ConfigurationProperty("name", IsKey = true, IsRequired = true)]
         public string Name
         {
-            get { return (string) base["name"]; }
-            set { base["name"] = value; }
+            get => (string) base["name"];
+            set => base["name"] = value;
         }
 
         [ConfigurationProperty("url", IsRequired = true)]
         public string Url
         {
-            get { return (string) base["url"]; }
-            set { base["url"] = value; }
+            get => (string) base["url"];
+            set => base["url"] = value;
         }
 
         [ConfigurationProperty("username", IsRequired = false)]
         public string Username
         {
-            get { return (string) base["username"]; }
-            set { base["username"] = value; }
+            get => (string) base["username"];
+            set => base["username"] = value;
         }
 
         [ConfigurationProperty("password", IsRequired = false)]
         public string Password
         {
-            get { return (string)base["password"]; }
-            set { base["password"] = value; }
+            get => (string)base["password"];
+            set => base["password"] = value;
         }
 
-        [ConfigurationProperty("type", IsRequired = false)]
+        [ConfigurationProperty("type", IsRequired = false, DefaultValue = "General")]
         public string Type
         {
-            get { return (string)base["type"]; }
-            set { base["password"] = value; }
+            get => (string)base["type"];
+            set => base["password"] = value;
         }
     }
 }
