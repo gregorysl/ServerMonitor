50d549a4db2075448e58843c5d71926aa0ad6ad1
unknown <gregorysl@gmail.com>
2018-07-11 22:39:51 +0200

    change objects to strings, add Type

--- _server/ServerMonitor/Models/Link.cs
+++ _server/ServerMonitor/Models/Link.cs
@@ -5,13 +5,15 @@ namespace ServerMonitor.Models
     public class Link
     {
         [JsonProperty("key")]
-        public object Name { get; set; }
+        public string Name { get; set; }
         [JsonProperty("working")]
         public bool Working { get; set; }
         [JsonProperty("message")]
-        public object Message { get; set; }
+        public string Message { get; set; }
+        [JsonProperty("type")]
+        public string Type { get; set; }
         [JsonProperty("url")]
-        public object Url { get; set; }
+        public string Url { get; set; }
         
     }
 }
