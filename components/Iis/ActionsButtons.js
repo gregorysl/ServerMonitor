e85acf1f7dbbb800d9678dce30b62c6142516c65
unknown <gregorysl@gmail.com>
2018-07-25 12:34:18 +0200

    remove whitelist button, as it moved to different column

--- components/Iis/ActionsButtons.js
+++ components/Iis/ActionsButtons.js
@@ -2,19 +2,16 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import StartStopButton from './StartStopButton';
-import { setIisAction, whitelistApp } from '../../actions/actions';
-import WhitelistButton from './WhitelistButton';
+import { setIisAction } from '../../actions/actions';
 
 
 const flatten = list => list.reduce((a, b) => a.concat(Array.isArray(b) ? flatten(b) : b), []);
 
 const ActionsButtons = (props) => {
   const onSet = () => props.set(props);
-  const onWhitelist = () => props.whitelist(props);
   return (
     <div className="actions-main">
       <StartStopButton running={props.running} click={onSet} />
-      <WhitelistButton whitelisted={props.whitelisted} click={onWhitelist} />
     </div>
   );
 };
@@ -27,22 +24,12 @@ const mapDispatchToProps = dispatch => ({
       condition: item.running
     };
     dispatch(setIisAction(data));
-  },
-  whitelist: (item) => {
-    const data =
-    {
-      appPools: flatten(item.apps.map(x => x.children)),
-      condition: item.whitelisted
-    };
-    dispatch(whitelistApp(data));
   }
 });
 
 ActionsButtons.propTypes = {
   running: PropTypes.bool.isRequired,
-  whitelisted: PropTypes.bool.isRequired,
-  set: PropTypes.func.isRequired,
-  whitelist: PropTypes.func.isRequired
+  set: PropTypes.func.isRequired
 };
 
 export default connect(null, mapDispatchToProps)(ActionsButtons);
