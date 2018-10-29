b5629aeec2864738db4450afc5cdc2c5e7e03c5f
Grzegorz Slazak <gregorysl@gmail.com>
2018-10-29 22:31:05 +0100

    showing total size of hardware as tag

--- components/Chart/chart.js
+++ components/Chart/chart.js
@@ -1,15 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Progress, Card } from 'antd';
+import { Progress, Card, Tag } from 'antd';
+import filesize from 'filesize';
 
-const Chart = ({ title, percent }) => (
-  <Card title={title} >
+const Title = ({ title, text }) => (
+  <div>
+    {title}
+    {text && <Tag>{filesize(text)}</Tag>}
+  </div>
+);
+Title.defaultProps = { text: '' };
+
+Title.propTypes = {
+  title: PropTypes.string.isRequired,
+  text: PropTypes.string
+};
+
+const Chart = ({ title, percent, text }) => (
+  <Card className="chart-card" title={(<Title text={text} title={title} />)}>
     <Progress type="dashboard" percent={percent} />
   </Card>
 );
+Chart.defaultProps = { text: '' };
 
 Chart.propTypes = {
   title: PropTypes.string.isRequired,
-  percent: PropTypes.number.isRequired
+  percent: PropTypes.number.isRequired,
+  text: PropTypes.string
 };
 export default Chart;
