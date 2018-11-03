0ad62d7fc735ea6456f1594de221db86c5c4d315
Grzegorz Slazak <gregorysl@gmail.com>
2018-11-03 22:43:28 +0100

    use react-table to show nested tables

--- components/Iis/AppPoolList.js
+++ components/Iis/AppPoolList.js
@@ -1,35 +1,46 @@
 import React from 'react';
-import { Table } from 'antd';
+import ReactTable from 'react-table';
+import 'react-table/react-table.css';
 import PropTypes from 'prop-types';
 import TooltipButon from '../TooltipButon';
 
-
-const columns = [
-  { title: 'key', dataIndex: 'key', key: 'key' },
-  {
-    title: 'State', dataIndex: 'running', key: 'running', render: running => (running ? 'Started' : 'Stopped')
-  },
-  {
-    title: 'Action', dataIndex: '', key: 'x', render: () => <TooltipButon title="recycle" icon="reload" />
-  }
-];
-
 const AppPoolList = (props) => {
   if (props.items.length === 0) {
-    return (<h1>No IIS applications found.</h1>);
+    return <h1>No IIS applications found.</h1>;
   }
-  props.items.forEach((element) => {
-    if (element.children) {
-      const childs = element.children.map(x => ({ key: x }));
-      element.children = childs;
-    }
-  });
   return (
-    <Table
-      showHeader={false}
-      columns={columns}
-      dataSource={props.items}
-      pagination={false}
+    <ReactTable
+      showPagination={false}
+      minRows={1}
+      data={props.items}
+      columns={[
+        {
+          Header: 'Name',
+          accessor: 'name'
+        },
+        {
+          Header: 'State',
+          accessor: 'running',
+          Cell: row => (
+            <font className={`state-${row.value ? 'started' : 'stopped'}`}>
+              {row.value ? 'Started' : 'Stopped'}
+            </font>
+          )
+        },
+        {
+          Header: 'Action',
+          accessor: 'x',
+          Cell: row => <TooltipButon title="recycle" icon="reload" />,
+          width: 100
+        }
+      ]}
+      SubComponent={row => (
+        <div className="app-pool-children">
+          {row.original.children.map(x => (
+            <span key={x}>{x}</span>
+          ))}
+        </div>
+      )}
     />
   );
 };
