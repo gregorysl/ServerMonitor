d5b76d11b9cf394604418e563c8250b3d4807727
Grzegorz Slazak <gregorysl@gmail.com>
2018-12-11 21:55:00 +0100

    remove unnecessary files from cra

--- Web/src/index.js
+++ Web/src/index.js
@@ -2,10 +2,8 @@ import 'antd/dist/antd.less';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import './index.css';
 import App from './App';
 import store from './store';
-import * as serviceWorker from './serviceWorker';
 import './styles/style.less';
 
 const Item = (
@@ -14,7 +12,3 @@ const Item = (
     </Provider>);
 ReactDOM.render(Item, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
