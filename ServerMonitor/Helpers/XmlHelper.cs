01c4e511f15aaf164a9358b25bbecda27daad6a2
Grzegorz Slazak <gregorysl@gmail.com>
2019-02-18 00:15:45 +0100

    Added whitelist class and helper for parsing

---
+++ ServerMonitor/Helpers/XmlHelper.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace ServerMonitor.Helpers
+{
+    public static class XmlHelper
+    {
+        public static List<string> GetAllValuesByNode(this XDocument document, string node)
+        {
+            return document.Descendants(node).First().Descendants("app").Select(x => x.Attribute("value")?.Value).ToList();
+        }
+    }
+}
---
+++ ServerMonitor/Models/Whitelist.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ServerMonitor.Models
+{
+    public class Whitelist
+    {
+        public List<string> Builds { get; set; } = new List<string>();
+        public List<string> Services { get; set; } = new List<string>();
+    }
+}
