22a72b2c63a2a2b830f8e5c354844dfde1a631b4
gregorysl <gregorysl@gmail.com>
2019-03-02 23:46:36 +0100

    get settings on page

--- Web/src/Settings.js
+++ Web/src/Settings.js
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useEffect  } from "react";
+import { connect } from "react-redux";
+import * as actions from "./actions/actions";
 
-const Settings = ({ item }) => {
+const Settings = (props) => {
+  useEffect(() => {
+    props.getSettings();
+  }, []);
   return (
     <div style={{ background: "#fff", padding: 5, height: "100%" }}>
       <h1>Settings</h1>
+      <p>{JSON.stringify(props.settings.data)}</p>
     </div>
   );
 };
 
-export default Settings;
+const mapStateToProps = state => ({
+  settings: state.settings
+});
+
+const mapDispatchToProps = dispatch => ({
+  getSettings: () => dispatch(actions.getSettings())
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Settings);
