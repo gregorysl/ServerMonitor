52a650eca12878f460e1b98918e41c68aae3c063
gregorysl <gregorysl@gmail.com>
2019-03-02 23:46:20 +0100

    new Settings api backend

---
+++ ServerMonitor/Controllers/SettingsController.cs
@@ -0,0 +1,19 @@
+﻿using ServerMonitor.Helpers;
+using ServerMonitor.Models;
+using System.Web.Http;
+
+namespace ServerMonitor.Controllers
+{
+    [RoutePrefix("Settings")]
+    public class SettingsController : BaseApi
+    {
+        [Route]
+        public Response Get()
+        {
+            var response = new Response();
+            response.Data = Settings;
+            return response;
+
+        }
+    }
+}
