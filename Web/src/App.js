c743fba8deaf4c2871cd3be94ac1b5671d852847
gregorysl <gregorysl@gmail.com>
2019-03-04 16:07:22 +0100

    add couple form fields

--- Web/src/App.js
+++ Web/src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 import { Icon, Layout } from "antd";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Home from "./Home";
 import Settings from "./Settings";
+import * as actions from "./actions/actions";
 
 const { Header, Content } = Layout;
 
 class App extends Component {
+  componentDidMount() {
+    this.props.getSettings();
+  }
   render() {
     return (
       <Router>
@@ -14,15 +19,20 @@ class App extends Component {
           <Header>
             <div className="logo" />
 
-            <Link className="text-colored" to="/">Home</Link>
+            <Link className="text-colored" to="/">
+              Home
+            </Link>
 
-            <Link className='settings' to="/settings">
+            <Link className="settings" to="/settings">
               <Icon type="setting" />
             </Link>
           </Header>
           <Content style={{ padding: "0 50px" }}>
             <Route exact path="/" component={Home} />
-            <Route path="/settings" component={Settings} />
+            <Route
+              path="/settings"
+              render={() => <Settings settings={this.props.settings.data} />}
+            />
           </Content>
         </Layout>
       </Router>
@@ -30,4 +40,16 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapStateToProps = state => (
+  {
+    settings: state.settings
+  });
+
+const mapDispatchToProps = dispatch => ({
+  getSettings: () => dispatch(actions.getSettings())
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
--- Web/src/Settings.js
+++ Web/src/Settings.js
@@ -1,28 +1,59 @@
-import React, { useEffect  } from "react";
-import { connect } from "react-redux";
-import * as actions from "./actions/actions";
+import React from "react";
+import { Form, Icon, Input,  Checkbox } from 'antd';
 
 const Settings = (props) => {
-  useEffect(() => {
-    props.getSettings();
-  }, []);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        console.log('Received values of form: ', values);
+      }
+    });
+  }
+  const { getFieldDecorator } = props.form;
   return (
     <div style={{ background: "#fff", padding: 5, height: "100%" }}>
       <h1>Settings</h1>
-      <p>{JSON.stringify(props.settings.data)}</p>
+      <span>{JSON.stringify(props.settings, null, 4)}</span>
+      
+      <Form onSubmit={handleSubmit} className="login-form">
+        <Form.Item>
+          {getFieldDecorator('commonAppName', {
+            rules: [{ required: true }]
+          })(
+            <Input placeholder="commonAppName" />
+          )}
+        </Form.Item>
+        <Form.Item>
+          {getFieldDecorator('cacheInSeconds', {
+            rules: [{ required: true }]
+          })(
+            <Input placeholder="cacheInSeconds" />
+          )}
+        </Form.Item>
+        <Form.Item label="Oracle Instance Manager Enabled">
+          {getFieldDecorator('isOracleInstanceManagerEnabled', {
+            valuePropName: "checked",
+            rules: [{ required: true }]
+          })(
+            <Checkbox  />
+          )}
+        </Form.Item>
+      </Form>
     </div>
   );
 };
 
-const mapStateToProps = state => ({
-  settings: state.settings
-});
 
-const mapDispatchToProps = dispatch => ({
-  getSettings: () => dispatch(actions.getSettings())
-});
+export default Form.create({
+  mapPropsToFields(props) {
+    const formData = {};
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Settings);
+    if (props.settings != null) {
+      Object.entries(props.settings).forEach(([k, v]) => {
+        formData[k] = Form.createFormField({ value: v });
+      });
+    }
+    return formData;
+  }
+})(Settings);
