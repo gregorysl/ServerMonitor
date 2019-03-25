93ab19bdd3f4951d12af9ece434a533816c81b13
gregorysl <gregorysl@gmail.com>
2019-03-25 12:35:19 +0100

    Links component to function component

--- Web/src/components/servicesList.js
+++ Web/src/components/servicesList.js
@@ -1,51 +1,30 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
+import React, { useState } from "react";
 import { Card, Row, Col } from "antd";
-import PropTypes from "prop-types";
 import ServiceItem from "./ServiceItem";
-import { getServicesAction } from "../actions/actions";
 
 function onlyUnique(value, index, self) {
   return self.indexOf(value) === index;
 }
 
-class ServicesList extends Component {
-  componentDidMount() {
-    this.props.dispatch(getServicesAction());
-  }
-
-  render() {
-    const cards = [];
-    const types = this.props.service.data.map(x => x.type).filter(onlyUnique);
-    types.forEach(element => {
-      const items = this.props.service.data.filter(x => x.type === element);
-      const inner = items.map(x => <ServiceItem {...x} key={x.name} />);
-      const columnCard = (
-        <Col xs={24} sm={12} md={12} lg={6} key={element}>
-          <Card title={element}>{inner}</Card>
-        </Col>
-      );
-      cards.push(columnCard);
-    });
-    return (
-      <React.Fragment>
-        <h1>Component Status</h1>
-        <Row>{cards}</Row>
-      </React.Fragment>
+const ServicesList = props => {
+  const cards = [];
+  const types = props.links.data.map(x => x.type).filter(onlyUnique);
+  types.forEach(element => {
+    const items = props.links.data.filter(x => x.type === element);
+    const inner = items.map(x => <ServiceItem {...x} key={x.name} />);
+    const columnCard = (
+      <Col xs={24} sm={12} md={12} lg={6} key={element}>
+        <Card title={element}>{inner}</Card>
+      </Col>
     );
-  }
-}
-
-const mapStateToProps = state => ({
-  service: state.service
-});
-
-ServicesList.propTypes = {
-  dispatch: PropTypes.func.isRequired,
-  service: PropTypes.shape({
-    loading: PropTypes.bool.isRequired,
-    data: PropTypes.array.isRequired
-  }).isRequired
+    cards.push(columnCard);
+  });
+  return (
+    <React.Fragment>
+      <h1>Component Status</h1>
+      <Row>{cards}</Row>
+    </React.Fragment>
+  );
 };
 
-export default connect(mapStateToProps)(ServicesList);
+export default ServicesList;
