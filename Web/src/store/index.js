c0df259170a41b0b04add3a0271f3df485a7a04f
gregorysl <gregorysl@gmail.com>
2019-03-30 22:48:21 +0100

    enable redux dev tools

--- Web/src/store/index.js
+++ Web/src/store/index.js
@@ -1,20 +1,17 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import rootReducer from '../reducers/reducers';
 import sagas from '../sagas/sagas';
 
-const configureStore = () => {
-  const sagaMiddleware = createSagaMiddleware();
-  return {
-    ...createStore(
-      rootReducer,
-      applyMiddleware(sagaMiddleware),
-    ),
-    runSaga: sagaMiddleware.run
-  };
-};
 
-const store = configureStore();
-store.runSaga(sagas);
+const sagaMiddleware = createSagaMiddleware();
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(sagaMiddleware))
+);
+
+sagaMiddleware.run(sagas);
 
 export default store;
