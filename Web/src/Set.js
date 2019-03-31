d1d9b72af8cb8c3293d3e25397747184d2ce80a4
gregorysl <gregorysl@gmail.com>
2019-03-31 14:23:57 +0200

    Extract common components for from fields

--- Web/src/Set.js
+++ Web/src/Set.js
@@ -8,92 +8,47 @@ import { Button, Input, Row, Col } from "antd";
 const renderField = ({ input, label, type }) => (
   <Input {...input} type={type} placeholder={label} />
 );
+const ColField = ({ name, component, label, md }) => (
+  <Col sm={24} md={md}>
+    <Field
+      name={name}
+      component={component ? component : renderField}
+      label={label}
+    />
+  </Col>
+);
+const RemoveButton = ({fields,index})=>(<Col sm={24} md={2}>
+  <Button onClick={() => fields.remove(index)} type='danger'>
+    Remove
+  </Button>
+</Col>);
+
 const linksSection = ({ fields }) =>
-fields.map((member, index) => (
-  <Row key={index}>
-    <Col sm={24} md={3}>
-      <Field
-        name={`${member}.name`}
-        component={renderField}
-        label='name'
-      />
-    </Col>
-    <Col sm={24} md={9}>
-      <Field
-        name={`${member}.url`}
-        component={renderField}
-        label='url'
-      />
-    </Col>
-    <Col sm={24} md={3}>
-      <Field
-        name={`${member}.type`}
-        component={renderField}
-        label='type'
-      />
-    </Col>
-    <Col sm={24} md={3}>
-      <Field
-        name={`${member}.username`}
-        component={renderField}
-        label='username'
-      />
-    </Col>
-    <Col sm={24} md={4}>
-      <Field
-        name={`${member}.password`}
-        component={renderField}
-        label='password'
-      />
-    </Col>
-    <Col sm={24} md={2}>
-      <Button onClick={() => fields.remove(index)} type='danger'>
-        Remove
-      </Button>
-    </Col>
-  </Row>
-));
+  fields.map((member, index) => (
+    <Row key={index}>
+      <ColField name={`${member}.name`} label='name' md={3} />
+      <ColField name={`${member}.url`} label='url' md={9} />
+      <ColField name={`${member}.type`} label='name' md={3} />
+      <ColField name={`${member}.username`} label='username' md={3} />
+      <ColField name={`${member}.password`} label='password' md={4} />
+      <RemoveButton fields={fields} index={index} />
+    </Row>
+  ));
 const hardwareSection = ({ fields }) =>
-fields.map((member, index) => (
-  <Row key={index}>
-    <Col sm={24} md={3}>
-      <Field
-        name={`${member}.name`}
-        component={renderField}
-        label='name'
-      />
-    </Col>
-    <Col sm={24} md={9}>
-      <Field
-        name={`${member}.url`}
-        component={renderField}
-        label='url'
-      />
-    </Col>
-    <Col sm={24} md={2}>
-      <Button onClick={() => fields.remove(index)} type='danger'>
-        Remove
-      </Button>
-    </Col>
-  </Row>
-));
+  fields.map((member, index) => (
+    <Row key={index}>
+      <ColField name={`${member}.name`} label='name' md={3} />
+      <ColField name={`${member}.url`} label='url' md={9} />
+      <RemoveButton fields={fields} index={index} />
+    </Row>
+  ));
 const dirsSection = ({ fields }) =>
-fields.map((member, index) => (
-  <Row key={index}>
-    <Col sm={24} md={3}>
-      <Field
-        name={`${member}`}
-        component={renderField}
-        label='name'
-      />
-    </Col>
-    <Col sm={24} md={2}>
-      <Button onClick={() => fields.remove(index)} type='danger'>
-        Remove
-      </Button>
-    </Col>
-  </Row>
-));
+  fields.map((member, index) => (
+    <Row key={index}>
+      <ColField name={`${member}`} label='name' md={22} />
+      <RemoveButton fields={fields} index={index} />
+    </Row>
+  ));
 const onSubmit = async values => {
   window.alert(JSON.stringify(values, 0, 2));
 };
@@ -117,22 +72,14 @@ let Sets = props => (
     }) => {
       return (
         <form onSubmit={handleSubmit}>
-          <Button onClick={() => push("links", {})}>Add Link</Button>
-          <FieldArray name='links'>
-            {linksSection}
-          </FieldArray>
           <Button onClick={() => push("hardwareList", {})}>Add Link</Button>
-          <FieldArray name='hardwareList'>
-            {hardwareSection}
-          </FieldArray>
+          <FieldArray name='hardwareList'>{hardwareSection}</FieldArray>
+          <Button onClick={() => push("links", {})}>Add Link</Button>
+          <FieldArray name='links'>{linksSection}</FieldArray>
           <Button onClick={() => push("dirsToCheckSize", "")}>Add Link</Button>
-          <FieldArray name='dirsToCheckSize'>
-            {dirsSection}
-          </FieldArray>
+          <FieldArray name='dirsToCheckSize'>{dirsSection}</FieldArray>
           <Button onClick={() => push("scheduledTasks", "")}>Add Link</Button>
-          <FieldArray name='scheduledTasks'>
-            {dirsSection}
-          </FieldArray>
+          <FieldArray name='scheduledTasks'>{dirsSection}</FieldArray>
 
           <div className='buttons'>
             <button type='submit' disabled={submitting || pristine}>
