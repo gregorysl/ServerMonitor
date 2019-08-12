7887cf742d2739de8a7941a9a46db0eb3c2ca49e
gregorysl <gregorysl@gmail.com>
2019-08-12 20:19:28 +0200

    Commit with HashRouter changes

--- Web/src/App.js
+++ Web/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Icon, Layout } from "antd";
-import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Link, Switch } from "react-router-dom";
 import Home from "./Home";
 import Settings from "./Settings";
 import * as actions from "./actions/actions";
