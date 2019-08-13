576aa8c0dfde00bda14dc964fc5f6f50cd059412
gregorysl <gregorysl@gmail.com>
2019-08-13 19:36:56 +0200

    simplify component

--- Web/src/components/Iis/IisMaster.js
+++ Web/src/components/Iis/IisMaster.js
@@ -1,12 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { Row } from "antd";
 import { getAllUrls } from "../../api/api_new";
-import ReactTable from "react-table";
 import "react-table/react-table.css";
 import IisSection from "./IisSection";
 
 const commonName = "?iisCommonName=";
-const noExpander = () => ({ "data-qnt": 0 });
 
 const IisServicesList = props => {
   const [iisData, setIisData] = useState([]);
@@ -31,27 +29,16 @@ const IisServicesList = props => {
     fetchData(urls);
   }, [forceRefresh, props.settings]);
 
-  const expandedRows = iisData.map((x, i) => i + 1);
+  const data = iisData.map(x => (
+    <Row key={x.key}>
+      <h3>{x.key}</h3>
+      <IisSection iisData={x} refresh={setforceRefresh} />
+    </Row>
+  ));
   return (
     <Row>
       <h1 className='table-title'>IIS Applications</h1>
-      <ReactTable
-        minRows={0}
-        showPagination={false}
-        TheadComponent={()=>null}
-        noDataText='No IIS applications found'
-        getTrProps={noExpander}
-        data={iisData}
-        columns={[
-          {
-            Header: "Name",
-            accessor: "key",
-            Cell: row => <h1>{row.value}</h1>
-          }
-        ]}
-        expanded={expandedRows}
-        SubComponent={row => <IisSection iisData={row.original} refresh={setforceRefresh} />}
-      />
+      {data}
     </Row>
   );
 };
