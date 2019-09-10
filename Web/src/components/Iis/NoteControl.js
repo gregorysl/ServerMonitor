ea398eadbfa0b93e46c50994012c564788a9fd66
gregorysl <gregorysl@gmail.com>
2019-09-10 23:47:44 +0200

    convert NoteControl to function component, use hooks and fix errors

--- Web/src/components/Iis/NoteControl.js
+++ Web/src/components/Iis/NoteControl.js
@@ -1,71 +1,44 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { setIisAction } from "../../actions/actions";
 import { Input, Icon } from "antd";
 
-class NoteControl extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: props.org.note
-    };
-    this.confirmNote = this.confirmNote.bind(this);
-    this.setEditMode = this.setEditMode.bind(this);
-    this.handleKeyPress = this.handleKeyPress.bind(this);
-  }
+const NoteControl = props => {
+  const [note, setValue] = useState(props.org.note);
+  const dispatch = useDispatch();
+  const iconFill = !note || note.length === 0 ? null : "filled";
 
-  setEditMode() {
-    this.setState({
-      value: this.props.org.note
-    });
-  }
-
-  updateInputValue(evt) {
-    this.setState({
-      value: evt.target.value
-    });
-  }
-
-  handleKeyPress(event) {
+  const handleKeyPress = event => {
     if (event.key === "Enter") {
-      this.confirmNote();
+      confirmNote();
     } else if (event.keyCode === 27) {
-      this.setEditMode();
+      setValue(props.org.note);
     }
-  }
-  confirmNote() {
-    if (this.props.org.note !== this.state.value) {
-      this.props.org.note = this.state.value;
-      this.props.click(
-        this.props.org,
-        this.props.url,
-        this.props.refresh,
-        "Note"
-      );
+  };
+  const confirmNote = () => {
+    if (props.org.note !== note) {
+      props.org.note = note;
+      dispatch(setIisAction({ build: props.org, action: "Note" }, props.url));
     }
-  }
-
-  render() {
-    const noteProp = this.props.org.note;
-    const iconFill = !noteProp || noteProp.length === 0 ? null : "filled";
-    return (
-      <Input
-        className="tag-input clear-input"
-        type="text"
-        value={this.state.value}
-        onPressEnter={this.confirmNote}
-        onBlur={this.confirmNote}
-        onChange={evt => this.updateInputValue(evt)}
-        onKeyDown={this.handleKeyPress}
-        onClick={this.setEditMode}
-        prefix={
-          <Icon
-            style={{ fontSize: "25px", marginLeft: "-10px" }}
-            type="message"
-            theme={iconFill}
-          />
-        }
-      />
-    );
-  }
-}
+  };
+  return (
+    <Input
+      className="tag-input clear-input"
+      type="text"
+      value={note}
+      onPressEnter={confirmNote}
+      onBlur={confirmNote}
+      onChange={evt => setValue(evt.target.note)}
+      onKeyDown={handleKeyPress}
+      prefix={
+        <Icon
+          style={{ fontSize: "25px", marginLeft: "-10px" }}
+          type="message"
+          theme={iconFill}
+        />
+      }
+    />
+  );
+};
 
 export default NoteControl;
