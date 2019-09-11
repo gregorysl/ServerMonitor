8396dddadb7fdc1d968d5b817550dd079ba97b9c
gregorysl <gregorysl@gmail.com>
2019-09-11 21:19:16 +0200

    merge action button with session panel

--- Web/src/components/SessionsPanel.js
+++ Web/src/components/SessionsPanel.js
@@ -2,19 +2,21 @@ import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import * as actions from "../actions/actions";
 import DataTable from "./Iis/DataTable";
-import SessionsActionButtons from "./SessionsActionButtons";
+import TooltipButton from "./TooltipButton";
 
 const SessionsPanel = props => {
   const dispatch = useDispatch();
   const sessions = useSelector(state => state.sessions);
   const sessionActions = {
     accessor: "x",
-    Cell: row => (
-      <SessionsActionButtons
-        click={() => dispatch(actions.killSession(row.original.id))}
-        state={row.original.state}
-      />
-    ),
+    Cell: row =>
+      row.original.state !== "Active" && (
+        <TooltipButton
+          click={() => dispatch(actions.killSession(row.original.id))}
+          icon="user-delete"
+          tooltip="Log off user"
+        />
+      ),
     width: 100
   };
   if (!sessions.loaded) {
