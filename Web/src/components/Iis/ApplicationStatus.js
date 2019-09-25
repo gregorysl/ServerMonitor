5d6b06d493daf0295be3dc3328c8cb3396ec3d4f
gregorysl <gregorysl@gmail.com>
2019-09-25 16:02:29 +0200

    Don't show tag when application is running

--- Web/src/components/Iis/ApplicationStatus.js
+++ Web/src/components/Iis/ApplicationStatus.js
@@ -3,26 +3,13 @@ import React from "react";
 import { Tag, Icon, Tooltip } from "antd";
 
 const ApplicationStatus = props => {
-  const color =
-    props.state === "Running"
-      ? "green"
-      : props.state === "Stopped"
-      ? "red"
-      : "gold";
+  const color = props.state === "Stopped" ? "red" : "gold";
   return (
-    <>
-      <Tag color={color}>{props.state}</Tag>
-      {props.cleanerMark && (
-        <Tooltip title="This build matches criteria to be cleaned on next run">
-          <Icon className="icon-large" type="warning" />
-        </Tooltip>
-      )}
-      {props.name && props.pool && props.name !== props.pool && (
-        <Tooltip title="Application pool has different name than IIS application. Check your configuration for errors">
-          <Icon className="icon-large" type="warning" />
-        </Tooltip>
-      )}
-    </>
+    props.state !== "Running" && (
+      <Tag width={100} color={color}>
+        {props.state}
+      </Tag>
+    )
   );
 };
 export default ApplicationStatus;
