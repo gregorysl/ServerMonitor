f078c28a437b765ad57b849d52ba18761ab56726
gregorysl <gregorysl@gmail.com>
2019-09-26 14:13:29 +0200

    Change DataTable table to antd one

--- Web/src/components/Iis/DataTable.js
+++ Web/src/components/Iis/DataTable.js
@@ -1,46 +1,31 @@
 import React from "react";
-import ReactTable from "react-table";
+import { Table } from "antd";
 import PropTypes from "prop-types";
 
-const DataTable = ({
-  data,
-  title,
-  columns,
-  loading,
-  extraColumns,
-  message
-}) => {
+const DataTable = ({ title, data, columns, extraColumns, loaded }) => {
   const finalColumns = [...columns, ...extraColumns];
   return (
-    data.length !== 0 && (
-      <React.Fragment>
-        <h1 className="table-title">{title}</h1>
-        {message !== "" && <h2>{message}</h2>}
-        <ReactTable
-          showPagination={false}
-          sortable={false}
-          loading={loading}
-          data={data}
-          columns={finalColumns}
-          minRows={1}
-        />
-      </React.Fragment>
-    )
+    <React.Fragment>
+      <h1 className="table-title">{title}</h1>
+      <Table
+        loading={!loaded}
+        rowKey="id"
+        pagination={false}
+        columns={finalColumns}
+        dataSource={data}
+      />
+    </React.Fragment>
   );
 };
 
 DataTable.defaultProps = {
-  extraColumns: [],
-  loading: false,
-  message: ""
+  extraColumns: []
 };
 
 DataTable.propTypes = {
   data: PropTypes.arrayOf(PropTypes.object).isRequired,
   columns: PropTypes.arrayOf(PropTypes.object).isRequired,
   title: PropTypes.string.isRequired,
-  message: PropTypes.string,
-  loading: PropTypes.bool,
   extraColumns: PropTypes.arrayOf(PropTypes.object)
 };
 
