faa3d570723bbaf71a803f4fa058258783009f73
gregorysl <gregorysl@gmail.com>
2019-10-21 11:26:31 +0200

    styling of new hardware charts

--- Web/src/components/Iis/IisMaster.js
+++ Web/src/components/Iis/IisMaster.js
@@ -1,17 +1,27 @@
 import React from "react";
-import { Row } from "antd";
+import { Row, Col } from "antd";
 import ServerLinks from "../Links/ServerLinks";
+import Hardware from "../Hardware";
 import IisSection from "./IisSection";
 
 const IisServicesList = props => {
   const data = props.settings.hardwareList.map(x => (
-    <Row key={x.name} style={{ marginBottom: 10 }}>
-      <span className="app-title">{x.name}</span>
-      <ServerLinks key={x.name} url={x.url} />
+    <div key={x.name} style={{ marginBottom: 10 }}>
+      <Row type="flex" justify="left">
+        <Row>
+          <Row>
+            <span className="app-title">{x.name}</span>
+          </Row>
+          <Row>
+            <ServerLinks key={x.name} url={x.url} />
+          </Row>
+        </Row>
+        <Hardware name={x.name} url={x.url} />
+      </Row>
       <Row>
         <IisSection name={x.name} url={x.url} />
       </Row>
-    </Row>
+    </div>
   ));
   return (
     <Row>
