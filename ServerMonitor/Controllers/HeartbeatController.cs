50f43fa94be2fad8abe004bf3a4e344661c4c34d
gregorysl <gregorysl@gmail.com>
2019-10-21 14:19:14 +0200

    Add HeartbeatController

---
+++ ServerMonitor/Controllers/HeartbeatController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Http;
+using System.Web.Http.Results;
+
+namespace ServerMonitor.Controllers
+{
+    [RoutePrefix("heartbeat")]
+    public class HeartbeatController : ApiController
+    {
+        public OkResult Get()
+        {
+            return Ok();
+        }
+
+    }
+}
