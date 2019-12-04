5a658eff02de7ba581c9a77c6942aff7e30b71a9
gregorysl <gregorysl@gmail.com>
2019-12-04 14:33:17 +0100

    Load more data only if server is working

--- Web/src/components/Iis/IisMaster.js
+++ Web/src/components/Iis/IisMaster.js
@@ -1,27 +1,39 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Row } from "antd";
 import ServerLinks from "../Links/ServerLinks";
 import Hardware from "../Hardware";
 import IisSection from "./IisSection";
+import { getHeartbeat } from "../../actions/actions";
+const A = ({ name, url }) => {
+  const heartbeat = useSelector(state => state.heartbeat[name]);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (!heartbeat || (!heartbeat.loaded && !heartbeat.loading)) {
+      dispatch(getHeartbeat(name, url));
+    }
+  }, [dispatch, heartbeat, name, url]);
 
-const IisServicesList = props => {
-  const data = props.settings.hardwareList.map(x => (
-    <div key={x.name} style={{ marginBottom: 5 }}>
-      <Row type="flex" justify="left">
+  if (!heartbeat) return null;
+  const shouldLoad = heartbeat.working;
+  return (
+    <div style={{ marginBottom: 5 }}>
+      <Row type="flex" justify="start">
         <Row>
           <Row>
-            <span className="app-title">{x.name}</span>
+            <span className="app-title">{name}</span>
           </Row>
-          <Row>
-            <ServerLinks key={x.name} url={x.url} />
-          </Row>
+          <Row>{shouldLoad && <ServerLinks url={url} />}</Row>
         </Row>
-        <Hardware name={x.name} url={x.url} />
+        {shouldLoad && <Hardware name={name} url={url} />}
       </Row>
-      <Row>
-        <IisSection name={x.name} url={x.url} />
-      </Row>
+      <Row>{shouldLoad && <IisSection name={name} url={url} />}</Row>
     </div>
+  );
+};
+const IisServicesList = props => {
+  const data = props.settings.hardwareList.map(x => (
+    <A key={x.name} name={x.name} url={x.url} />
   ));
   return (
     <Row>
