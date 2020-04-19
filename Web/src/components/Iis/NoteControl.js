6d99c404f00f7133c0b566d7e4c089b7e31cedd3
gregorysl <gregorysl@gmail.com>
2020-04-19 22:24:03 +0200

    updated note control

--- Web/src/components/Iis/NoteControl.js
+++ Web/src/components/Iis/NoteControl.js
@@ -1,25 +1,81 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch } from "react-redux";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import InputBase from "@material-ui/core/InputBase";
+import IconButton from "@material-ui/core/IconButton";
+import CheckIcon from "@material-ui/icons/Check";
+import CloseIcon from "@material-ui/icons/Close";
 import { setIisAction } from "../../actions/actions";
-import { Typography } from "antd";
 
-const { Paragraph } = Typography;
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: "flex",
+    alignItems: "center",
+    height: 44
+  },
+  input: {
+    marginLeft: theme.spacing(1),
+    flex: 1
+  },
+  iconButton: {
+    padding: 10
+  }
+}));
 
-const NoteControl = ({ org, url }) => {
+const NoteControl = ({ build, url, isEdit, setIsEdit }) => {
+  const [note, setNote] = useState(build.note);
   const dispatch = useDispatch();
+  const classes = useStyles();
 
-  const confirmNote = value => {
-    if (org.note !== value) {
-      org.note = value;
-      dispatch(setIisAction({ build: org, action: "Note" }, url));
+  const confirm = () => {
+    if (build.note !== note) {
+      build.note = note;
+      dispatch(setIisAction({ build, action: "Note" }, url));
+    }
+    setIsEdit(false);
+  };
+
+  const cancel = () => {
+    setNote(build.note);
+    setIsEdit(false);
+  };
+
+  const _handleKeyDown = e => {
+    if (e.key === "Enter") {
+      confirm();
+      return;
+    } else if (e.key === "Escape") {
+      cancel();
+      return;
     }
   };
-  const noteToShow = !org.note || org.note === "" ? "No description" : org.note;
+  const handleChange = event => {
+    setNote(event.target.value);
+  };
 
   return (
-    <Paragraph className="build-note" editable={{ onChange: confirmNote }}>
-      {noteToShow}
-    </Paragraph>
+    <Paper elevation={isEdit ? 1 : 0} className={classes.root}>
+      <InputBase
+        autoFocus
+        disabled={!isEdit}
+        className={classes.input}
+        placeholder="Build note"
+        value={note}
+        onKeyDown={_handleKeyDown}
+        onChange={handleChange}
+      />
+      {isEdit && (
+        <IconButton onClick={confirm} className={classes.iconButton}>
+          <CheckIcon />
+        </IconButton>
+      )}
+      {isEdit && (
+        <IconButton onClick={cancel} className={classes.iconButton}>
+          <CloseIcon />
+        </IconButton>
+      )}
+    </Paper>
   );
 };
 
