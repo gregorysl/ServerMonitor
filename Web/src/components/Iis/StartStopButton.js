af85826257fbcdf9ee7f35046eaa021815371c8b
gregorysl <gregorysl@gmail.com>
2020-04-19 21:57:58 +0200

    redesign start stop buttons

--- Web/src/components/Iis/StartStopButton.js
+++ Web/src/components/Iis/StartStopButton.js
@@ -1,14 +1,18 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Switch } from "antd";
+import TooltipIcon from "../TooltipIcon";
+import PlayArrowIcon from "@material-ui/icons/PlayArrow";
+import PauseIcon from "@material-ui/icons/Pause";
 
 const StartStopButton = ({ running, click, build }) => {
+  const tooltip = running ? "Stop" : "Start";
+  const icon = running ? <PauseIcon /> : <PlayArrowIcon />;
   const toggle = () =>
     click({
       build,
       action: "Toggle"
     });
-  return <Switch checked={running} onChange={toggle} />;
+  return <TooltipIcon tooltip={tooltip} click={toggle} icon={icon} />;
 };
 
 StartStopButton.propTypes = {
