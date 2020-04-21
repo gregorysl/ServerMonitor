955f1a77e8b8d3d5b31ac753d3e381eda23a3a63
gregorysl <gregorysl@gmail.com>
2020-04-22 00:01:38 +0200

    rewrite settings

--- Web/src/Settings.js
+++ Web/src/Settings.js
@@ -4,83 +4,75 @@ import { Form, Field } from "react-final-form";
 import arrayMutators from "final-form-arrays";
 import { setSettings } from "./actions/actions";
 import { FieldArray } from "react-final-form-arrays";
-import { PlusOutlined, DeleteOutlined } from "@ant-design/icons";
+import DeleteOutlineIcon from "@material-ui/icons/DeleteOutline";
+import AddIcon from "@material-ui/icons/Add";
+import Input from "@material-ui/core/Input";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardContent from "@material-ui/core/CardContent";
+import TextField from "@material-ui/core/TextField";
+import Grid from "@material-ui/core/Grid";
 
-import {
-  Button,
-  Input,
-  Row,
-  Col,
-  Checkbox,
-  InputNumber,
-  PageHeader
-} from "antd";
-
+const fieldInput = ({ input, label, type }) => (
+  <TextField {...input} label={label} />
+);
 const renderField = ({ input, label, type }) => (
-  <Input {...input} type={type} placeholder={label} />
-);
-const numberField = ({ input, label, type }) => (
-  <InputNumber {...input} type={type} placeholder={label} />
+  <Input fullWidth {...input} type={type} placeholder={label} />
 );
 
-const checkboxComponent = ({ input: { onChange, value }, meta, ...rest }) => (
-  <Checkbox
-    {...rest}
-    checked={!!value}
-    onChange={(e, isChkd) => onChange(isChkd)}
-  >
-    Use whitelist
-  </Checkbox>
-);
 const ColField = ({ name, component, label, md }) => (
-  <Col xs={24} sm={12} md={md}>
+  <Grid item xs={12} sm={6} md={md}>
     <Field
       name={name}
       component={component ? component : renderField}
       label={label}
     />
-  </Col>
+  </Grid>
 );
 const RemoveButton = ({ fields, index }) => (
-  <Col xs={24} sm={12} md={2}>
-    <Button
-      icon={<DeleteOutlined />}
-      onClick={() => fields.remove(index)}
-      type="danger"
-    ></Button>
-  </Col>
+  <Grid item xs={12} sm={6} md={1}>
+    <IconButton aria-label="delete" onClick={() => fields.remove(index)}>
+      <DeleteOutlineIcon />
+    </IconButton>
+  </Grid>
 );
 const AddButton = ({ push, name, newItem = {} }) => (
-  <Button icon={<PlusOutlined />} onClick={() => push(name, newItem)}>
+  <Button
+    variant="contained"
+    size="small"
+    onClick={() => push(name, newItem)}
+    startIcon={<AddIcon />}
+  >
     Add
   </Button>
 );
 
 const linksSection = ({ fields }) =>
   fields.map((member, index) => (
-    <Row key={index}>
-      <ColField name={`${member}.name`} label="name" md={3} />
-      <ColField name={`${member}.url`} label="url" md={9} />
-      <ColField name={`${member}.type`} label="name" md={3} />
-      <ColField name={`${member}.username`} label="username" md={3} />
-      <ColField name={`${member}.password`} label="password" md={4} />
+    <Grid container spacing={3} key={index}>
+      <ColField name={`${member}.name`} label="name" md={1} />
+      <ColField name={`${member}.url`} label="url" md={6} />
+      <ColField name={`${member}.username`} label="username" md={2} />
+      <ColField name={`${member}.password`} label="password" md={2} />
       <RemoveButton fields={fields} index={index} />
-    </Row>
+    </Grid>
   ));
 const hardwareSection = ({ fields }) =>
   fields.map((member, index) => (
-    <Row key={index}>
+    <Grid container spacing={3} key={index}>
       <ColField name={`${member}.name`} label="name" md={3} />
-      <ColField name={`${member}.url`} label="url" md={9} />
+      <ColField name={`${member}.url`} label="url" md={8} />
       <RemoveButton fields={fields} index={index} />
-    </Row>
+    </Grid>
   ));
 const dirsSection = ({ fields }) =>
   fields.map((member, index) => (
-    <Row key={index}>
-      <ColField name={`${member}`} label="name" md={22} />
+    <Grid container spacing={3} key={index}>
+      <ColField name={`${member}`} label="name" md={11} />
       <RemoveButton fields={fields} index={index} />
-    </Row>
+    </Grid>
   ));
 
 let Settings = props => {
@@ -110,84 +102,86 @@ let Settings = props => {
             : "cleaner.jsonWhitelistPath";
         return (
           <form onSubmit={handleSubmit}>
-            <PageHeader title="Cleaner configuration">
-              <Row>
-                <label>Cleaner can remove builds older than (days): </label>
-                <Field name={"cleaner.beforeDays"} component={numberField} />
-              </Row>
-              <Row>
-                <label>
-                  Exclude following number of latest builds from being cleaned:{" "}
-                </label>
-                <Field name={"cleaner.excludeNLast"} component={numberField} />
-              </Row>
-              <Field
-                name={"cleaner.useWhiteList"}
-                component={checkboxComponent}
+            <Card>
+              <CardHeader
+                title="Additional servers"
+                subheader="(this will add more tabs to Hardware section)"
+                action={<AddButton push={push} name="hardwareList" />}
               />
-              <Row>
+              <CardContent>
+                <FieldArray name="hardwareList">{hardwareSection}</FieldArray>
+              </CardContent>
+            </Card>
+            <Card>
+              <CardHeader
+                title="Components to check"
+                subheader="(add data for services avaibility you want to check)"
+                action={<AddButton push={push} name="links" />}
+              />
+              <CardContent>
+                <FieldArray name="links">{linksSection}</FieldArray>
+              </CardContent>
+            </Card>
+            <Card>
+              <CardHeader
+                title="Directories"
+                subheader="(will check size occupied)"
+                action={
+                  <AddButton push={push} name="dirsToCheckSize" newItem={""} />
+                }
+              />
+              <CardContent>
+                <FieldArray name="dirsToCheckSize">{dirsSection}</FieldArray>
+              </CardContent>
+            </Card>
+            <Card>
+              <CardHeader
+                action={
+                  <AddButton push={push} name="scheduledTasks" newItem={""} />
+                }
+                title="Scheduled tasks"
+              />
+              <CardContent>
+                <FieldArray name="scheduledTasks">{dirsSection}</FieldArray>
+              </CardContent>
+            </Card>
+            <Card>
+              <CardHeader title="Additional Settings" />
+              <CardContent>
+                <Field
+                  label="Group apps by"
+                  name="commonAppName"
+                  component={fieldInput}
+                  type="text"
+                />
                 <label>Whitelist type: </label>
                 <Field name="cleaner.whitelistType" component="select">
                   <option value={0}>XML</option>
                   <option value={1}>JSON</option>
                 </Field>
-              </Row>
-              <label>Path: </label>
-              <Field name={pathType} component={renderField} type="text" />
-            </PageHeader>
-            <PageHeader
-              title="Additional servers"
-              subTitle="(this will add more tabs to Hardware section)"
-              extra={<AddButton push={push} name="hardwareList" />}
+                <Field
+                  label="Whitelist path"
+                  name={pathType}
+                  component={fieldInput}
+                  type="text"
+                />
+              </CardContent>
+            </Card>
+            <Button
+              variant="contained"
+              type="submit"
+              color="primary"
+              disabled={submitting || pristine}
             >
-              <FieldArray name="hardwareList">{hardwareSection}</FieldArray>
-            </PageHeader>
-            <PageHeader
-              title="Components to check"
-              subTitle="(add data for services avaibility you want to check)"
-              extra={<AddButton push={push} name="links" />}
+              Save settings
+            </Button>
+            <Button
+              variant="contained"
+              onClick={reset}
+              disabled={submitting || pristine}
             >
-              <FieldArray name="links">{linksSection}</FieldArray>
-            </PageHeader>
-            <PageHeader
-              title="Directories"
-              subTitle="(will check size occupied)"
-              extra={<AddButton push={push} name="links" />}
-            >
-              <FieldArray name="dirsToCheckSize">{dirsSection}</FieldArray>
-            </PageHeader>
-            <PageHeader
-              title="Scheduled tasks"
-              extra={
-                <AddButton push={push} name="scheduledTasks" newItem={""} />
-              }
-            >
-              <FieldArray name="scheduledTasks">{dirsSection}</FieldArray>
-            </PageHeader>
-            <PageHeader title="Additional Settings">
-              <label>Group Apps by following name: </label>
-              <Field name="commonAppName" component={renderField} type="text" />
-            </PageHeader>
-            <Row>
-              <Col sm={24} md={12}>
-                <Button
-                  type="primary"
-                  htmlType="submit"
-                  disabled={submitting || pristine}
-                >
-                  Save settings
-                </Button>
-              </Col>
-              <Col sm={24} md={12}>
-                <Button
-                  onClick={reset}
-                  disabled={submitting || pristine}
-                  type="danger"
-                >
-                  Reset
-                </Button>
-              </Col>
-            </Row>
+              Reset
+            </Button>
           </form>
         );
       }}
