778fa709375ba13fc434883b1e8ba18ceeae4738
gregorysl <gregorysl@gmail.com>
2020-04-23 17:06:38 +0200

    Change title location

--- Web/src/components/Iis/DataTable.js
+++ Web/src/components/Iis/DataTable.js
@@ -1,5 +1,6 @@
 import React from "react";
 import MaterialTable from "material-table";
+import Typography from "@material-ui/core/Typography";
 
 const DataTable = ({
   title,
@@ -10,19 +11,22 @@ const DataTable = ({
   rowKey
 }) => {
   return (
-    <MaterialTable
-      title={title}
-      columns={columns}
-      data={data}
-      actions={extraColumns}
-      options={{
-        paging: false,
-        actionsColumnIndex: -1,
-        search: false,
-        sorting: false,
-        draggable: false
-      }}
-    />
+    <>
+      <Typography variant="subtitle1">{title}</Typography>
+      <MaterialTable
+        columns={columns}
+        data={data}
+        actions={extraColumns}
+        options={{
+          paging: false,
+          actionsColumnIndex: -1,
+          search: false,
+          sorting: false,
+          draggable: false,
+          toolbar: false
+        }}
+      />
+    </>
   );
 };
 
