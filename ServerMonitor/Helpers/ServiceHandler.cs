732d97f1c2bd6a76eca2e9d2dde73f7543e9a30b
Artur Snioszek <artur.snioszek@fenergo.com>
2022-06-21 09:28:40 +0200

    Add possibility to start/stop service

---
+++ ServerMonitor/Helpers/ServiceHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using System.ServiceProcess;
+
+namespace ServerMonitor.Helpers
+{
+    public class ServiceHandler
+    {
+        public void Toggle(string appName)
+        {
+            var service = ServiceController.GetServices().FirstOrDefault(x => x.ServiceName.EndsWith(appName));
+
+            if (service?.Status == ServiceControllerStatus.Running)
+            {
+                service?.Stop();
+            }
+            else
+            {
+                service?.Start();
+            }
+        }
+    }
+}
