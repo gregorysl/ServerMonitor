98b74aea9b2217d50aa4d352441d35bda714e1c4
BigYellowHammer <6392942+BigYellowHammer@users.noreply.github.com>
2024-09-20 13:45:21 +0200

    Update .net code to use proper package versions

--- Web/src/components/Iis/ServerData.js
+++ Web/src/components/Iis/ServerData.js
@@ -9,7 +9,6 @@ import Card from "@material-ui/core/Card";
 import CardHeader from "@material-ui/core/CardHeader";
 import CardContent from "@material-ui/core/CardContent";
 import { makeStyles } from "@material-ui/core/styles";
-import Typography from "@material-ui/core/Typography";
 import { Grid } from "@material-ui/core";
 import Link from "@material-ui/core/Link";
 
